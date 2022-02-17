b373df13ad4d1df36e103acb3c1a7a1913554a99
Mukan Atazhanov <atazhanovmukan@gmail.com>
2022-02-17 01:52:38 +0100

    Fixed

--- src/main/java/test/task/tasks/Controller.java
+++ src/main/java/test/task/tasks/Controller.java
@@ -51,43 +51,4 @@ public class Controller {
         }
         return reversed.toString();
     }
-
-    /*Výpočet provede postupně takto:
-
-      - všechny číslice menší 3 (včetně) posune o jednu pozici doprava. Např: 43256791 => 45326791
-
-      - všechny číslice 8 a 9 vynásobí 2. Např.: 45326791 => 453267181
-
-      - všechny číslice 7 smaže: Např: 453267181 => 45326181
-
-      - ve výsledném čísle spočte počet sudých číslic a tímto počtem výsledné číslo vydělí a
-      zaokrouhlí dolů na celá čísla. Např: 45326181 / 4 => 11331545
-
-    Pro zadané číslo 43256791 je výsledek 11331545.*/
-
-    //todo switching pos and finding amount of even number
-    @GetMapping("/solve2")
-    public String num(@RequestParam(value = "number", defaultValue = "43256791") Integer number) {
-        String temp = Integer.toString(number);
-        int[] intArr = new int[temp.length()];
-        int evenNum = 0;
-        StringBuilder res = new StringBuilder();
-        for(int i = 0; i < temp.length(); i++) {
-            intArr[i] = temp.charAt(i) - '0';
-        }
-        for(int j : intArr) {
-            switch (j) {
-                case 8:
-                case 9:
-                    res.append(j * 2);
-                    break;
-                case 7:
-                    continue;
-                default:
-                    res.append(j);
-            }
-        }
-
-        return res.toString();
-    }
 }
